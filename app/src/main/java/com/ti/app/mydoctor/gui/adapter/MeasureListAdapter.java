9b19981d0afaa52813e75df687e6e214fd9834a3
SO000228 <massimo.martini@noovle.com>
2018-10-23 14:44:38 +0000

    Bug fix icona urgente in cancellazione misura (M.Martini)
    git-svn-id: http://10.188.40.144:8080/TeleMedicina/Trunk/tlm-gtw/tlm-gtw-android-core@513425 f6862068-0fce-11de-802a-13e956f158ca

--- app/src/main/java/com/ti/app/mydoctor/gui/adapter/MeasureListAdapter.java
+++ app/src/main/java/com/ti/app/mydoctor/gui/adapter/MeasureListAdapter.java
@@ -39,7 +39,6 @@ public class MeasureListAdapter extends SimpleAdapter {
 			String strIcon = (String) data.get(position).get(from[0]);
 			ImageView imageItem = v.findViewById(to[0]);
 			imageItem.setImageResource(Integer.valueOf(strIcon));
-
 			holder.measureDetailsIcon = imageItem;
 			
 			String strTopLabel = (String) data.get(position).get(from[1]);
@@ -60,12 +59,10 @@ public class MeasureListAdapter extends SimpleAdapter {
             }
 			holder.measureTopLabelTV = topLabel;
 
-			if (from.length > 2) {
-				String strBottomLabel = (String) data.get(position).get(from[2]);
-				GWTextView bottomLabel = v.findViewById(to[2]);
-				bottomLabel.setText(strBottomLabel);
-				holder.measureBottomLabelTV = bottomLabel;
-			}
+            String strBottomLabel = (String) data.get(position).get(from[2]);
+            GWTextView bottomLabel = v.findViewById(to[2]);
+            bottomLabel.setText(strBottomLabel);
+            holder.measureBottomLabelTV = bottomLabel;
 			
 			v.setTag(holder);
 	    }
@@ -75,10 +72,28 @@ public class MeasureListAdapter extends SimpleAdapter {
         holder.measureDetailsIcon.setImageResource(Integer.valueOf(icon));
 	    String topLbl = (String) data.get(position).get(from[1]);
         holder.measureTopLabelTV.setText(topLbl);
-        if (from.length > 2) {
-            String bottomLbl = (String) data.get(position).get(from[2]);
-            holder.measureBottomLabelTV.setText(bottomLbl);
-        }
+		String urgent = (String) data.get(position).get(from[3]);
+		if ("true".equalsIgnoreCase(urgent)) {
+			Drawable drawable = ContextCompat.getDrawable(context, R.drawable.urgent);
+			int size = (int) Math.round(holder.measureTopLabelTV.getLineHeight() * 0.75); // The percentage you like (0.8, 0.9, etc.)
+			drawable.setBounds(0, 0, size, size); // setBounds(int left, int top, int right, int bottom), in this case, drawable is a square image
+			holder.measureTopLabelTV.setCompoundDrawablePadding(size / 2);
+			holder.measureTopLabelTV.setCompoundDrawables(
+			        null, //left
+					null, //top
+                    drawable, //right
+					null //bottom
+			);
+		} else
+            holder.measureTopLabelTV.setCompoundDrawables(
+                    null, //left
+                    null, //top
+                    null, //right
+                    null //bottom
+            );
+        String bottomLbl = (String) data.get(position).get(from[2]);
+        holder.measureBottomLabelTV.setText(bottomLbl);
+
 	    return v;
 	}
 	
